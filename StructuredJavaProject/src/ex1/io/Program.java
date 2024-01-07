5d65e0e6d01d29c48783729330997b1c7c682b82
da0293 <dy7133@naver.com>
2024-01-07 16:11:22 +0900

    commit

---
+++ StructuredJavaProject/src/ex1/io/Program.java
@@ -0,0 +1,31 @@
+package ex1.io;
+/*
+ * 저작권정보..
+ * 작성자 :
+ * 버전 :
+ */
+public class Program {
+
+	public static void main(String[] args) {
+		int kor1, kor2, kor3; 
+		int total; 
+		float avg;
+		
+		kor1=50; 
+		kor2=60; 
+		kor3=80; 
+		
+		total = kor1 + kor2 + kor3;
+		avg = total/3; 
+		
+		/* 
+		 	성적 출력 부분 
+		 */
+		System.out.println("┌──────────────────┐");
+		System.out.println("│     성적 출력       │");
+		System.out.println("└──────────────────┘");
+		
+
+	}
+
+}
