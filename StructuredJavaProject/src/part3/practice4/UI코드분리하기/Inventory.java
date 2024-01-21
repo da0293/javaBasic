f75286a17e29e62ab3698d21e497e011b7103e83
da0293 <dy7133@naver.com>
2024-01-22 00:48:57 +0900

    commit UI코드 분리

--- StructuredJavaProject/src/part3/practice4/UI코드분리하기/Inventory.java
+++ StructuredJavaProject/src/part3/practice4/UI코드분리하기/Inventory.java
@@ -1,7 +1,5 @@
 package part3.practice4.UI코드분리하기;
 
-import java.util.Scanner;
-
 public class Inventory {
 	private Product[] products;
 	private int current; 
@@ -14,70 +12,44 @@ public class Inventory {
 		this.products = new Product[size]; 
 		this.current=0; 
 	}
-	
-	public void addInventory() {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("상품 입력");
-		
-		String name; 
-		int price, quantity;
-		
-		System.out.printf("상품 이름 : "); 
-		name=sc.next();
-		do {
-			System.out.print("가격 : ");
-			price=sc.nextInt();
-			if(price<0) {
-				System.out.println("가격은 0보다 커야합니다.");
-			}
-		}while(price<0);
-		do {
-			System.out.print("수량 : ");
-			quantity=sc.nextInt();
-			if(quantity<0) {
-				System.out.println("수량은 0보다 커야합니다.");
-			}
-		}while(quantity<0);
-		
-		Product product= new Product(name, price, quantity); 
-		Product[] products = this.products; 
-		int size = this.current; 
+
+	public Product[] getProducts() {
+		return products;
+	}
+
+	public void setProducts(Product[] products) {
+		this.products = products;
+	}
+
+	public int getCurrent() {
+		return current;
+	}
+
+	public void setCurrent(int current) {
+		this.current = current;
+	}
+
+	public int size() {
+		return this.current;
+	}
+
+	// 상품을 재고목록에 추가 
+	public void add(Product product) {
+		Product[] products = this.products;
+		int size = this.size(); 
 		if(products.length==size) {
-			// 1.크기가 3개정도 더 큰 새로운 배열을 생성
 			Product[] temp = new Product[size+3]; 
-			// 2.값을 이주 
 			for( int i=0; i<size; i++) {
 				temp[i]=products[i]; 
 			}
-			// 3.inventory.products가 새로 만든 temp배열을 참조
 			products = temp;
-			 /*
-			 이 부분 추가 
-			 temp 배열을 products에 할당했지만, 이는 지역 변수에 할당된 것이기 때문에 메소드가 종료되면서 사라지게 됩니다 
-			 따라서 this.products에 다시 할당
-			 */
 			this.products=products;
 		}
 		products[size]=product;
 		current++;
 	}
-	public void displayInventory() {
-		// 집중화 코드 
-		this.displayInventory(this.current);
-	}
-	
-	// overload
-	// 넘겨받은 size값만큼 출력
-	public void displayInventory(int size) {
-		System.out.println("상품 출력");
-		for( int i=0; i<size; i++) {
-			Product product = this.products[i]; 
-			String name = product.getName(); 
-			int price = product.getPrice();
-			int quantity = product.getQuantity();
-			System.out.println( "상품 이름 : " + name);
-			System.out.println( "상품 가격 : " + price);
-			System.out.println( "상품 수량 : " + quantity);
-		}		
+
+	public Product get(int i) {
+		return products[i];
 	}
 }
