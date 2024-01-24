170b86a76c5639a622628bf7db0a1347542c8107
da0293 <dy7133@naver.com>
2024-01-25 01:38:18 +0900

    commit 16 Override(우선순위가 높은) 메소드

---
+++ OOPJavaPrj/src/NewlecExam.java
@@ -0,0 +1,23 @@
+import part3.ex4.UI코드분리하기.Exam;
+
+public class NewlecExam extends Exam {
+	private int com;
+
+	public int getCom() {
+		return com;
+	}
+
+	public void setCom(int com) {
+		this.com = com;
+	} 
+	@Override
+	public int total() {
+		return super.total()+com;
+	}
+	
+	@Override
+	public float avg() {
+		return this.total()/4.0f;
+	}
+	
+}
