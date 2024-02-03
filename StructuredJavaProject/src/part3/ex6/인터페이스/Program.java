627ec9c088dc13e826e530f0f312c0950b36fe8d
da0293 <dy7133@naver.com>
2024-02-03 17:57:25 +0900

    commit 33 새로운 객체로 바꾸기(문자열을 읽어서 객체로 만들기)

--- StructuredJavaProject/src/part3/ex6/인터페이스/Program.java
+++ StructuredJavaProject/src/part3/ex6/인터페이스/Program.java
@@ -1,11 +1,28 @@
 package part3.ex6.인터페이스;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Program {
 
-	public static void main(String[] args) {
+
+	public static void main(String[] args) throws ClassNotFoundException, IOException, InstantiationException, IllegalAccessException {
+		// setting.txt를 읽어오기위한 파일 입력
+		FileInputStream fis = new FileInputStream("src/part3/ex6/인터페이스/setting.txt");
+		Scanner scan = new Scanner(fis);
+		String className = scan.nextLine();
+		// System.out.println(className); 
+		scan.close();
+		fis.close();
+		
+		// 문자열을 이용해 클래스 정보 얻기 
+		Class clazz = Class.forName(className);
 		A a = new A();
-		B b = new B(); 
-		a.setX(b);
+		// 클래스정보를 통해 새로운 인터페이스를 만들 수 있다. 
+		// 그러므로 메서드를 통해 인터페이스를 만들 수 있다. 
+		X x = (X) clazz.newInstance();
+		a.setX(x);
 		a.print();
 	}
 
